3f1dbde30c13821493d6d1b938855f220f5139d8
rounith <112827481+rounith@users.noreply.github.com>
2022-09-07 00:18:27 +0530

    Create Chef On Date.java

---
+++ CodeChef/Chef On Date.java
@@ -0,0 +1,26 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		int i;
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(i=0;i<n;i++)
+		{
+		    int x=sc.nextInt();
+		    int y=sc.nextInt();
+		    if(x>=y)
+		    System.out.println("Yes");
+		    else
+		    System.out.println("No");
+		}
+	}
+}
