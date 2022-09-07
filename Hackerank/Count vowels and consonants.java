042ce409d721a28c9df2b03cd765d6abadf7a0d1
rounith <112827481+rounith@users.noreply.github.com>
2022-09-07 23:26:13 +0530

    Create Count vowels and consonants.java

---
+++ Hackerank/Count vowels and consonants.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int ccount=0,vcount=0;
+        String str=sc.next();
+        for(int i=0;i<str.length();i++)
+        {
+          if(str.charAt(i)=='a'||str.charAt(i)=='e'||str.charAt(i)=='i'||str.charAt(i)=='o'||str.charAt(i)=='u'||str.charAt(i)=='A'||str.charAt(i)=='E'||str.charAt(i)=='I'||str.charAt(i)=='O'||str.charAt(i)=='U')
+                vcount++;
+            else
+                ccount++;
+        }
+        System.out.println(vcount+" "+ccount);
+    }
+}
